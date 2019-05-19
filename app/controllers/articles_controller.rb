ffdad1a950e3240381650bc3db5d06fadc83711c
eightbeat <sansei.kid011@gmail.com>
2019-05-19 12:18:43 +0900

    Add admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,6 +44,7 @@ class ArticlesController < ApplicationController
   end
 
   private
+  
   def set_article
     @article = Article.find(params[:id])
   end
@@ -53,7 +54,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if @current_user != @article.user
+    if @current_user != @article.user and !current_user.admin?
       flash[:danger] = "You can only edit or destroy your own article"
       redirect_to root_path
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_same_user, omly: [:edit, :update]
+  before_action :require_same_user, omly: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -37,6 +38,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have beeb deleted"
+    redirect_to users_path
+  end
+
   private
 
   def user_params
@@ -48,8 +56,15 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action"
       redirect_to root_path
     end
   end
